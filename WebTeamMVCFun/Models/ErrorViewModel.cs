2b8633b8fede4ab75a3df79bf5a9b08a812e255e
MacMike <mgby@pm.me>
2022-08-03 15:17:50 -0700

    scaffold for MVC API and razor pages

---
+++ WebTeamMVCFun/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebTeamMVCFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
